c19ba9d8e8cb3d301085ccc27cc0022f5935ee1d
olinda25 <felipe.j.ruiz64@gmail.com>
2022-09-23 19:14:24 -0400

    added city, county , main lesson 26

---
+++ Java_Lesson26/City.java
@@ -0,0 +1,11 @@
+public class City {
+    public void hail() {
+        System.out.println("Hello from my City");
+    }
+
+    public void displayName() {
+        System.out.println("The City Name is:");
+    }
+    
+}
+j
---
+++ Java_Lesson26/County.java
@@ -0,0 +1,15 @@
+public class County extends City {
+    
+    public void hail() {
+        System.out.println("Hello from my County");
+    }
+
+    public void hail(String s) {
+        System.out.println("Hello from" + " my County");
+    }
+
+    public void displayName() {
+        System.out.println("The County Name is: " );   
+    
+    }
+}
