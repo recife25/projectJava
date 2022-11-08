1b855ce0352f8b5e2d37095ba61cb72fdec878a8
olinda25 <felipe.j.ruiz64@gmail.com>
2022-11-07 20:55:49 -0500

    added lesson28 lesson29

---
+++ lesson29/List.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Felipe Ruiz - aka Phil
+ * lesson29 
+ * Collections - list(arraylist, linkedlist)
+ * set(hashset)
+ * map(hashmap)
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List {
+
+   
+
+    public static void main(String[] args) {
+        
+        //ArrayList<String> myArray = new ArrayList<>();
+        List<String> myArray = new ArrayList<>(); //interface & class
+        
+    }
+    
+}
